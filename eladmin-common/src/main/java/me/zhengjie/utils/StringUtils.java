e59a558872f18c8f5616fd799a3a298b790a0902
kongfanyu <19388483@qq.com>
2020-12-02 23:43:02 +0800

    删除注释

--- eladmin-common/src/main/java/me/zhengjie/utils/StringUtils.java
+++ eladmin-common/src/main/java/me/zhengjie/utils/StringUtils.java
@@ -35,7 +35,6 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * @author Zheng Jie
  * 字符串工具类, 继承org.apache.commons.lang3.StringUtils类
  */
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
